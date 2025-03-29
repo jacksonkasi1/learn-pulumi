5380cc049336cbe92bda3c6da45dd191b75672fd
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2025-03-29 23:00:19 +0530

    Refactor PDF generation to use a separate function and update dependencies. Added esbuild for building TypeScript files and modified tsconfig for improved module interoperability. Updated Lambda function to return PDF as base64 encoded string with appropriate headers.

--- class-1/index.ts
+++ class-1/index.ts
@@ -1,8 +1,7 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as apigateway from "@pulumi/aws-apigateway";
 
-import * as PDFDocument from "pdfkit";
+import { createPdf } from "./dist/functions/pdf";
 
 // A Lambda function to invoke
 const fn = new aws.lambda.CallbackFunction("fn", {
@@ -17,43 +16,20 @@ const fn = new aws.lambda.CallbackFunction("fn", {
 const pdfFn = new aws.lambda.CallbackFunction("pdfFn", {
   memorySize: 512,
   callback: async (ev, ctx) => {
-    const createPdf = async () => {
-      return new Promise((resolve, reject) => {
-        const doc = new PDFDocument({
-          size: "A4",
-          margin: 50,
-        });
-
-        doc.registerFont("Helvetica", "Helvetica.ttf");
-        doc.registerFont("Helvetica-Bold", "Helvetica-Bold.ttf");
-
-        // Add a title
-        doc.font("Helvetica-Bold").fontSize(24).text("Hello World", 50, 50);
-
-        // Add a subtitle
-        doc.font("Helvetica").fontSize(18).text("This is a subtitle", 50, 100);
-
-        // Add a table
-        doc.font("Helvetica").fontSize(12).text("This is a table", 50, 150);
-
-        // Add a footer
-        doc.font("Helvetica").fontSize(12).text("This is a footer", 50, 200);
-
-        const buffer: Buffer[] = [];
-        doc.on("data", buffer.push.bind(buffer));
-        doc.on("end", () => {
-          resolve(Buffer.concat(buffer));
-        });
-        doc.end();
-      });
-    };
+    const pdf = await createPdf();
 
     return {
       statusCode: 200,
-      body: createPdf(),
+      body: pdf.toString("base64"),
+      headers: {
+        "Content-Type": "application/pdf",
+        "Content-Disposition": "attachment; filename=my-pdf.pdf",
+      },
+      isBase64Encoded: true,
     };
   },
 });
+
 // A REST API to route requests to HTML content and the Lambda function
 const api = new apigateway.RestAPI("api", {
   routes: [
